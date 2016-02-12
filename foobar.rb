d05f8d4c88474b2f97f39ed5c6cf1183371b4c92
Stephanie Lin <your.email@example.com>
2016-02-11 23:09:38 -0800

    hw1 done

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,17 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(a)
+  	list = []
+  	a.each do |i|
+  	  num = Integer(i)
+  	  if (num % 2 == 0) and (num+2<10) and (list.include?(num+2) == false)
+  	  	list += [num+2]
+  	  end
+  	end
+
+  	sum = 0
+  	list.each do |i|
+  	  sum += i
+  	end
+  	return sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,17 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a + b)*(a+b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  list = []
+  a.sort.each do |i|
+  	list += [i+1]
+  end
+  return list
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
